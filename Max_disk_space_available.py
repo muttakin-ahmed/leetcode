0298ca37c5f2aa0ccf74cb83d64f30c39bf52fba
Muttakin Ahmed Khan <muttakin-ahmed@users.noreply.github.com>
2021-04-12 13:54:27 -0500

    Update Max_disk_space_available.py

--- Max_disk_space_available.py
+++ Max_disk_space_available.py
@@ -1,5 +1,3 @@
-
-
 def max_disk_space(freeSpaces, blockSize):
     start = 0
     end = blockSize
@@ -12,3 +10,6 @@ def max_disk_space(freeSpaces, blockSize):
     return max(disk_sizes)
 
 print(max_disk_space([8,2,4,5], 2))
+
+# Time complexity: O(n), as we have to irerate the array freeSpaces once and the solution time is dependant on the size of that array.
+# Space complexity: O(n), as we have used an array to store all the possible disk sizes.
