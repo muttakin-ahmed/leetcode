6f7b4c752cad1e8909bb541877eaa7154aa4eeea
Muttakin Ahmed Khan <muttakin-ahmed@users.noreply.github.com>
2021-04-12 12:54:26 -0500

    Add files via upload

---
+++ Count_sum_num_string.py
@@ -0,0 +1,14 @@
+def count_int_from_str(s):
+    digit = ""
+    sum = 0
+    for i in range(len(s)):
+        ch = s[i]
+        if ch.isdigit():
+            digit += ch
+        elif digit:
+            sum += int(digit)
+            digit = ""
+    return sum + int(digit) if digit else sum
+
+
+print(count_int_from_str("go256sdfgd44"))
